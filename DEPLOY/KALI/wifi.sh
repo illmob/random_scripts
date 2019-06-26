3c8ed99169299b48f6c5ffa7b1403494e7ca5fcd
illmob <42990096+illmob@users.noreply.github.com>
2019-06-25 22:27:44 -0400

    Create wifi.sh

---
+++ DEPLOY/KALI/wifi.sh
@@ -0,0 +1,12 @@
+apt-get update
+apt-get install libcurl4-openssl-dev libssl-dev zlib1g-dev libpcap-dev
+cd /opt
+git clone https://github.com/ZerBea/hcxdumptool.git
+cd hcxdumptool
+make
+make install
+cd ..
+git clone https://github.com/ZerBea/hcxtools.git
+cd hcxtools
+make
+make install
