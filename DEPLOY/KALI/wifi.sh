2c33416cce9775514a86e722770e9c6c9b41fb73
illmob <42990096+illmob@users.noreply.github.com>
2019-07-30 20:58:25 -0400

    added y

--- DEPLOY/KALI/wifi.sh
+++ DEPLOY/KALI/wifi.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get install libcurl4-openssl-dev libssl-dev zlib1g-dev libpcap-dev
+apt-get install libcurl4-openssl-dev libssl-dev zlib1g-dev libpcap-dev -y
 cd /opt
 git clone https://github.com/ZerBea/hcxdumptool.git
 cd hcxdumptool
