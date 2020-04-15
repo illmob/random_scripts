c573ec8bd9fd3d8b33c2b5da938056c00a629157
illmob <42990096+illmob@users.noreply.github.com>
2020-04-14 22:27:44 -0400

    update for Kali 2020

--- GIT/GitShit.sh
+++ GIT/GitShit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #Collection of awesome shit to grab on an initial kali rollout
-# skip prompts in apt-upgrade, etc.
-export DEBIAN_FRONTEND=noninteractive
+printf '\n============================================================\n'
+printf '[+] Fx root colors\n'
+printf '============================================================\n\n' 
+echo "PS1='\[\033[1;31m\]\u@\h\[\033[1;34m\]\w:\[\033[0;37m\]\\$'" >> \root\.bashrc
+source ~/.bashrc
 
 printf '\n============================================================\n'
 printf '[+] update Kali\n'
@@ -10,30 +13,11 @@ apt get update && apt-get dist-upgrade -y
 apt autoremove -y && apt clean
 
 printf '\n============================================================\n'
-printf '[+] Change Password\n'
-printf '============================================================\n\n 
-echo -e "sup3rS3cr3tpassw0rd!\nsup3rS3cr3tpassw0rd!"|passwd root;history -d $(history 1)
-
-printf '\n============================================================\n'
 printf '[+] Change Default SSH Keys\n'
 printf '============================================================\n\n' 
 mkdir /etc/ssh/backup-keys
 mv /etc/ssh/ssh_host_* /etc/ssh/backup-keys
 dpkg-reconfigure openssh-server
-
-printf '\n============================================================\n'
-printf '[+] Disable Lockscreen & Screen Blanking & Sleep on AC\n'
-printf '============================================================\n\n' 
-gsettings set org.gnome.desktop.lockdown disable-lock-screen 'true'
-gsettings set org.gnome.desktop.session idle-delay 0
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
-#Do this just in case the other shit doesnt work
-systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
-
-printf '\n============================================================\n'
-printf '[+] Turn off Dash to Dock Intelligent Autohide\n'
-printf '============================================================\n\n' 
-dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed true
 
 printf '\n============================================================\n'
 printf '[+] Add support for 32bit stuff\n'
@@ -44,90 +28,113 @@ printf '\n============================================================\n'
 printf '[+] git cloning all the good shit\n'
 printf '============================================================\n\n'
 cd /opt
-git clone https://github.com/danielmiessler/SecLists.git
+
+printf '\n============================================================\n'
+printf '[+] Install Initial Recon Tools\n'
+printf '============================================================\n\n'
+pip install shodan
 git clone https://github.com/xillwillx/skiptracer.git && cd skiptracer && pip install -r requirements.txt && cd ..
-git clone https://github.com/DominicBreuker/pspy.git
-git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
+git clone https://github.com/pry0cc/Doh365.git
+
+printf '\n============================================================\n'
+printf '[+] Install Wordlists\n'
+printf '============================================================\n\n'
+git clone https://github.com/danielmiessler/SecLists.git
+gzip -dc < /usr/share/wordlists/rockyou.txt.gz > /usr/share/wordlists/rockyou.txt
+
+printf '\n============================================================\n'
+printf '[+] Install Foothold Tools\n'
+printf '============================================================\n\n'
 git clone https://github.com/trustedsec/unicorn.git
 git clone https://github.com/oddcod3/Phantom-Evasion.git
+git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git
+git clone https://github.com/danielbohannon/Invoke-Obfuscation.git
+git clone https://github.com/phackt/stager.dll.git
+git clone https://github.com/infosecn1nja/MaliciousMacroMSBuild.git
+git clone https://github.com/GreatSCT/GreatSCT.git
 
-#shells
-git clone https://github.com/byt3bl33d3r/SILENTTRINITY.git
-git clone https://github.com/lesnuages/hershell.git
-git clone https://github.com/mthbernardes/rsg.git
-git clone https://github.com/0x00-0x00/ShellPop.git && apt install python-argcomplete && cd ShellPop && pip install -r requirements.txt && python setup.py install && cd ..
-git clone https://github.com/antonioCoco/SharPyShell.git && cd SharPyShell && pip install -r requirements.txt && cd ..
+printf '\n============================================================\n'
+printf '[+] Install Network Discovery Tools\n'
+printf '============================================================\n\n'
+git clone https://github.com/lgandx/PCredz.git
+git clone https://github.com/k0fin/pktrecon.git && cd pktrecon && pip install -r requirements.txt && cd ..
+git clone https://github.com/DanMcInerney/net-creds.git
+git clone https://github.com/ropnop/windapsearch.git
+pip install python-ldap #or apt-get install python-ldap
+git clone https://github.com/dirkjanm/adidnsdump && cd adidnsdump && pip install . && cd ..
+pip install bloodhound
 
+printf '\n============================================================\n'
+printf '[+] Install Local Situational Awareness Tools\n'
+printf '============================================================\n\n'
+git clone https://github.com/DominicBreuker/pspy.git
+git clone https://github.com/diego-treitos/linux-smart-enumeration.git
 
-#internal Priv escalation
+printf '\n============================================================\n'
+printf '[+] Install Priv Escalation Tools\n'
+printf '============================================================\n\n'
+git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
 git clone https://github.com/pentestmonkey/unix-privesc-check.git
 git clone https://github.com/pentestmonkey/windows-privesc-check.git
 git clone https://github.com/411Hall/JAWS.git
 git clone https://github.com/InteliSecureLabs/Linux_Exploit_Suggester.git
 git clone https://github.com/rebootuser/LinEnum.git
-git clone https://github.com/PowerShellMafia/PowerSploit.git
-git clone https://github.com/hausec/ADAPE-Script.git
-git clone https://github.com/dirkjanm/ldapdomaindump.git && pip install ldap3 dnspython && cd ldapdomaindump && python setup.py install && cd ..
-git clone https://github.com/CroweCybersecurity/ad-ldap-enum.git && pip install python-ldap
+wget https://github.com/rasta-mouse/Watson/archive/2.0.zip && unzip 2.0.zip && rm 2.0.zip
 
-#Passive Pcap Tools - https://www.netresec.com/?page=PcapFiles
-git clone https://github.com/lgandx/PCredz.git
-git clone https://github.com/k0fin/pktrecon.git && cd pktrecon && pip install -r requirements.txt && cd ..
-git clone https://github.com/DanMcInerney/net-creds.git
+printf '\n============================================================\n'
+printf '[+] Install Credential Tools\n'
+printf '============================================================\n\n'
+#mimikatz
+mimikatz=$(curl -sL https://github.com/gentilkiwi/mimikatz/releases/latest | grep -i /mimikatz_trunk.zip| sed -n 's/.*href="\([^"]*\).*/\1/p')&& wget -T 5 https://github.com$mimikatz &&  unzip -o mimikatz_trunk.zip -d /opt/mimikatz && unset mimikatz && rm mimikatz_trunk.zip
+git clone https://github.com/Hackndo/lsassy.git && cd lsassy && python3 setup.py install && cd /opt
+git clone https://github.com/skelsec/pypykatz.git && cd pypykatz && python3 setup.py install && cd /opt
+git clone https://github.com/TarlogicSecurity/kerbrute && cd kerbrute && pip install -r requirements.txt && cd /opt
+git clone https://github.com/0x09AL/RdpThief.git
+git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries.git
+git clone https://github.com/Mr-Un1k0d3r/MiniDump.git
+git clone https://github.com/outflanknl/Dumpert.git
+git clone https://github.com/xFreed0m/RDPassSpray.git
+git clone https://github.com/realoriginal/ppdump-public.git
+git clone https://github.com/hoangprod/AndrewSpecial.git
 
-#Network Scanning
-git clone https://github.com/marco-lancini/goscan.git && cd goscan && chmod +x goscan && mv ./goscan /usr/local/bin/goscan && cd ..
-
-pip install shodan
+printf '\n============================================================\n'
+printf '[+] Install Extra Scripts\n'
+printf '============================================================\n\n'
 mkdir ~/scripts && cd ~/scripts
 wget https://raw.githubusercontent.com/superkojiman/onetwopunch/master/onetwopunch.sh
 wget https://github.com/d0nkeys/redteam/raw/master/scanners/nmap/scan.sh
 wget https://github.com/d0nkeys/redteam/raw/master/scanners/nmap/scan.parallel.sh
-
 mkdir ~/powershell && cd ~/powershell
 wget https://raw.githubusercontent.com/peewpw/Invoke-WCMDump/master/Invoke-WCMDump.ps1
 wget https://github.com/d0nkeys/redteam/raw/master/privilege-escalation/JuicyPotato.ps1
 wget https://github.com/d0nkeys/redteam/raw/master/privilege-escalation/JuicyPotato32.ps1
 wget https://github.com/d0nkeys/redteam/raw/master/lateral-movement/Run-As.ps1
+wget https://raw.githubusercontent.com/itm4n/PrivescCheck/master/Invoke-PrivescCheck.ps1
+wget https://github.com/Mr-Un1k0d3r/PoisonHandler/raw/master/Execute-PoisonHandler.ps1
+wget https://raw.githubusercontent.com/dafthack/DomainPasswordSpray/master/DomainPasswordSpray.ps1
+mkdir ~/csharp && cd ~/csharp
+wget https://github.com/cobbr/SharpSploit/archive/v1.5.zip && unzip v1.5.zip && rm v1.5.zip
 
 printf '\n============================================================\n'
-printf '[+] compressing privesc - extract with: tar -zxvf upc2.tar.gz\n'
+printf '[+] Install Lateral Movement Tools\n'
 printf '============================================================\n\n'
-tar -C /opt/unix-privesc-check -czf /opt/unix-privesc-check/upc2.tar.gz unix-privesc-check2
+gem install evil-winrm
+cd /opt
+git clone https://github.com/Mr-Un1k0d3r/SCShell.git
 
 printf '\n============================================================\n'
-printf '[+] Extract rockyou wordlist\n'
-printf '============================================================\n\n' 
-gzip -dc < /usr/share/wordlists/rockyou.txt.gz > /usr/share/wordlists/rockyou.txt
-
-#mimikatz
-mimikatz=$(curl -sL https://github.com/gentilkiwi/mimikatz/releases/latest | grep -i /mimikatz_trunk.zip| sed -n 's/.*href="\([^"]*\).*/\1/p')&& wget -T 5 https://github.com$mimikatz &&  unzip -o mimikatz_trunk.zip -d /opt/mimikatz && unset mimikatz && rm mimikatz_trunk.zip
-
-printf '\n============================================================\n'
-printf '[+] Installing Alfa Drivers,golang +env, gnome-screenshot\n'
-printf '[+] pip + env, patator, zmap, chromium, crackmapexec\n'
+printf '[+] Install Wireless Tools\n'
 printf '============================================================\n\n'
-apt-get -y install \
-    golang \
-    python-pip \
-    python3-dev \
-    python3-pip \
-    patator \
-    zmap \
-    chromium \
-    crackmapexec
-
-python2 -m pip install pipenv
-python3 -m pip install pipenv
-mkdir -p /root/go
-gopath_exp='export GOPATH="$HOME/.go"'
-path_exp='export PATH="/usr/local/go/bin:$GOPATH/bin:$PATH"'
-sed -i '/export GOPATH=.*/c\' ~/.profile
-sed -i '/export PATH=.*GOPATH.*/c\' ~/.profile
-echo $gopath_exp | tee -a "$HOME/.profile"
-grep -q -F "$path_exp" "$HOME/.profile" || echo $path_exp | tee -a "$HOME/.profile"
-. "$HOME/.profile"
-sed -i 's#Exec=/usr/bin/chromium %U#Exec=/usr/bin/chromium --no-sandbox %U#g' /usr/share/applications/chromium.desktop
+cd /opt
+# Deploy hcxtools
+apt-get update
+apt-get install libcurl4-openssl-dev libssl-dev zlib1g-dev libpcap-dev -y
+cd /opt
+git clone https://github.com/ZerBea/hcxdumptool.git && cd hcxdumptool && make && make install && cd ..
+git clone https://github.com/ZerBea/hcxtools.git && cd hcxtools && make && make install && cd ..
+apt-get install jq
+git clone https://github.com/staz0t/hashcatch
+git clone git clone https://github.com/InfamousSYN/rogue.git && cd ./rogue && python install.py ..
 
 printf '\n============================================================\n'
 printf '[+] Initializing Metasploit Database\n'
@@ -138,30 +145,6 @@ msfdb init
 
 printf '\n============================================================\n'
 printf '[+] Finalize.\n'
-printf '============================================================\n\n
+printf '============================================================\n\n'
 updatedb
 rmdir ~/Music ~/Public ~/Videos ~/Templates &>/dev/null
-
-/* Optional Installs
-printf '\n============================================================\n'
-printf '[+] Installing Sublime Text\n'
-printf '============================================================\n\n
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
-apt install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-apt update
-apt install sublime-text
-
-printf '\n============================================================\n'
-printf '[+] Installing Bloodhound\n'
-printf '============================================================\n\n'
-mkdir -p /usr/share/neo4j/logs /usr/share/neo4j/run
-grep '^root   soft    nofile' /etc/security/limits.conf || echo 'root   soft    nofile  40000
-root   hard    nofile  60000' >> /etc/security/limits.conf
-grep 'NEO4J_ULIMIT_NOFILE=60000' /etc/default/neo4j 2>/dev/null || echo 'NEO4J_ULIMIT_NOFILE=60000' >> /etc/default/neo4j
-apt-get install -y bloodhound
-neo4j start
-
-cd /opt
-git clone https://github.com/EmpireProject/Empire.git && cd Empire &&  ./setup/install.sh && cd ..
-*/
